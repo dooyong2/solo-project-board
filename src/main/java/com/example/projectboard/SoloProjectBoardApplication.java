482f6ff2fcdd218929a15d5428ea4935be1db54a
dooyong2 <asdffg725@naver.com>
2023-06-16 13:30:12 +0900

    #5 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    dependency setting :
    - spring web
    - actuator
    - devtools
    - lombok

---
+++ src/main/java/com/example/projectboard/SoloProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoloProjectBoardApplication.class, args);
+    }
+
+}
