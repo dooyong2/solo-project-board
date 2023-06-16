66dcc7802541e88db9acf7a12bbf073d33c8bb9f
dooyong2 <asdffg725@naver.com>
2023-06-17 02:17:20 +0900

    #7 - 도메인 클래스 설계 (게시글, 댓글) 수정
    도메인 정보를 설계
    기초적인 게시글과 댓글 표현이 가능하도록 함
    DB 와 연동을 위한 추가적인 표현은 추후에 업데이트

--- src/main/java/com/example/projectboard/domain/Article.java
+++ src/main/java/com/example/projectboard/domain/Article.java
@@ -7,5 +7,9 @@ public class Article {
     private String title; // 제목
     private String content; // 본문
     private String hashtag; // 해시태그
-    
+
+    private LocalDateTime createAt; // 생성일시
+    private String createBy; // 생성자
+    private LocalDateTime modifiedAt; // 수정일시
+    private String modifiedBy; // 수정자
 }
