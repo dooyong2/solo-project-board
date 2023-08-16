c96a1db1a5a01fc3c53e23aa8eba70db9e82c02f
dooyong2 <asdffg725@naver.com>
2023-08-16 11:28:52 +0900

    어노테이션 추가 및 코드리팩토링

--- src/main/java/com/example/projectboard/config/DataRestConfig.java
+++ src/main/java/com/example/projectboard/config/DataRestConfig.java
@@ -2,8 +2,10 @@ package com.example.projectboard.config;
 
 import com.example.projectboard.domain.UserAccount;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
+@Configuration
 public class DataRestConfig {
 
     @Bean
