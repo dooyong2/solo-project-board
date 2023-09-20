4bf1926cfbe9ef0ff52d11ec1227303fe29f6b58
dooyong2 <asdffg725@naver.com>
2023-09-20 17:08:23 +0900

    #54 - 도메인 변경점 반영 : 해시태그
    해시태그 엔티티가 추가되고, 이제 하나의 게시글에서 여러 개의 해시태그를 가질 수 있도록 설계를 변경함.
    때문에 변경점이 많이 발생되었으며, 커밋이 컴파일 에러 상태로 두지 않는 것이 좋을 것 같아 한 커밋에 모든 변경점을 담음.
    
    도메인 변경만 반영하고 기능은 아직 구현하지 않아 컴파일 에러는 발생하지 않지만 현재 테스트는 실패함.

--- src/main/java/com/example/projectboard/domain/Article.java
+++ src/main/java/com/example/projectboard/domain/Article.java
@@ -5,15 +5,12 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
-import java.util.LinkedHashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 @Getter
 @ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "title"),
-        @Index(columnList = "hashtag"),
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy"),
 })
@@ -39,8 +36,14 @@ public class Article extends AuditingFields {
     private String content; // 본문
 
 
-    @Setter
-    private String hashtag; // 해시태그
+    @ToString.Exclude
+    @JoinTable(
+            name = "article_hashtag",
+            joinColumns = @JoinColumn(name = "articleId"),
+            inverseJoinColumns = @JoinColumn(name = "hashtagId")
+    )
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private Set<Hashtag> hashtags = new LinkedHashSet<>();
 
     @OrderBy("createdAt DESC")
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
@@ -50,15 +53,26 @@ public class Article extends AuditingFields {
 
     protected Article() {}
 
-    private Article(UserAccount userAccount, String title, String content, String hashtag) {
+    private Article(UserAccount userAccount, String title, String content) {
         this.userAccount = userAccount;
         this.title = title;
         this.content = content;
-        this.hashtag = hashtag;
     }
 
-    public static Article of(UserAccount userAccount, String title, String content, String hashtag) {
-        return new Article(userAccount, title, content, hashtag);
+    public static Article of(UserAccount userAccount, String title, String content) {
+        return new Article(userAccount, title, content);
+    }
+
+    public void addHashtag(Hashtag hashtag) {
+        this.getHashtags().add(hashtag);
+    }
+
+    public void addHashtags(Collection<Hashtag> hashtags) {
+        this.getHashtags().addAll(hashtags);
+    }
+
+    public void clearHashtags() {
+        this.getHashtags().clear();
     }
 
     @Override
